180827f8178666ab430099ea5b7a1ae62f2f87c5
samnyce87 <samirmesmoudi@hotmail.com>
2023-02-17 10:16:29 +0100

    meme

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes,Route } from 'react-router-dom';
 
 import Accueil from './Pages/Accueil';
 import Gallerie from './Pages/Gallerie';
-import Erreur from './Pages/Erreur';
+
 import Apropos from './Pages/Apropos';
 import Portofolio from './Pages/Portofolio';
 import Services from './Pages/Services';
@@ -16,7 +16,7 @@ function App() {
   <Routes>
     <Route path='/mon-app' element={<Accueil/>}/>
     <Route path='/Gallerie' element={<Gallerie/>}/>
-    { <Route path='*' element={<Erreur/>}/> }
+  
     <Route path='/Apropos' element={<Apropos/>}/>
     <Route path='/Portofolio' element={<Portofolio/>}/>
     <Route path='/Services' element={<Services/>}/>
